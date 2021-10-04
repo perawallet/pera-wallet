0d503a9b816765197d07338364d12126448bcae9
jvontzv3 <90168533+jeremyv3@users.noreply.github.com>
2021-10-04 00:27:30 -0500

    Revert "Unresolved references for gen Direction classes"
    This reverts commit 140a6b79a7b69e2907c7a3e1d705aed45260b63c.

--- android/app/src/prod/java/com.algorand.android/utils/NodeList.kt
+++ android/app/src/prod/java/com.algorand.android/utils/NodeList.kt
@@ -6,26 +6,24 @@ import com.algorand.android.models.Node
 const val CURRENT_DEFAULT_NODE_LIST_VERSION = 14
 
 val defaultNodeList = listOf(
-        Node(
-                name = "MainNet",
-//        algodAddress = "https://node-mainnet.aws.algodev.network/",
-                algodAddress = "http://localhost:4001",
-                algodApiKey = BuildConfig.ALGORAND_API_KEY,
-//        indexerAddress = "https://indexer-mainnet.aws.algodev.network/",
-                indexerAddress = "http://localhost:8980",
-                indexerApiKey = BuildConfig.INDEXER_API_KEY,
-                isActive = true,
-                isAddedDefault = true,
-                networkSlug = MAINNET_NETWORK_SLUG
-        ),
-        Node(
-                name = "TestNet",
-                algodAddress = "https://node-testnet.aws.algodev.network/",
-                algodApiKey = BuildConfig.ALGORAND_API_KEY,
-                indexerAddress = "https://indexer-testnet.aws.algodev.network/",
-                indexerApiKey = BuildConfig.INDEXER_API_KEY,
-                isActive = false,
-                isAddedDefault = true,
-                networkSlug = TESTNET_NETWORK_SLUG
-        )
+    Node(
+        name = "MainNet",
+        algodAddress = "https://node-mainnet.aws.algodev.network/",
+        algodApiKey = BuildConfig.ALGORAND_API_KEY,
+        indexerAddress = "https://indexer-mainnet.aws.algodev.network/",
+        indexerApiKey = BuildConfig.INDEXER_API_KEY,
+        isActive = true,
+        isAddedDefault = true,
+        networkSlug = MAINNET_NETWORK_SLUG
+    ),
+    Node(
+        name = "TestNet",
+        algodAddress = "https://node-testnet.aws.algodev.network/",
+        algodApiKey = BuildConfig.ALGORAND_API_KEY,
+        indexerAddress = "https://indexer-testnet.aws.algodev.network/",
+        indexerApiKey = BuildConfig.INDEXER_API_KEY,
+        isActive = false,
+        isAddedDefault = true,
+        networkSlug = TESTNET_NETWORK_SLUG
+    )
 )
